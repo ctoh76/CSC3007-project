709a0213224c88fa316bdb1260b380fca49e47b3
ivantime <sayivanthreetimes@gmail.com>
2022-06-29 22:00:43 +0800

    Modified Slide Content and Re-Editted HTML/CSS/JS Scripts for Webpage

--- script.js
+++ script.js
@@ -3,7 +3,20 @@ function reveal() {
     for (var i = 0; i < reveals.length; i++) {
         var windowHeight = window.innerHeight;
         var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
+        var elementVisible = 200;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+function revealHover() {
+    var reveals = document.querySelectorAll(".revealHover");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 500;
         if (elementTop < windowHeight - elementVisible) {
             reveals[i].classList.add("active");
         } else {
@@ -12,3 +25,4 @@ function reveal() {
     }
 }
 window.addEventListener("scroll", reveal);
+window.addEventListener("scroll", revealHover);
